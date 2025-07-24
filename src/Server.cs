3be56d2b607f85ae81bcfc39c525c8ab24cd218a
Ryan Flores <rryanflorres@gmail.com>
2025-07-23 22:03:33 -0700

    add sending message

--- src/Server.cs
+++ src/Server.cs
@@ -1,6 +1,13 @@
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
-TcpListener server = new TcpListener(IPAddress.Any, 4221);
+TcpListener server = new (IPAddress.Any, 4221);
 server.Start();
 server.AcceptSocket(); // wait for client
+
+TcpClient client = server.AcceptTcpClient();
+
+string message = "HTTP/1.1 200 OK\r\n\r\n";
+
+client.Client.Send(Encoding.ASCII.GetBytes(message));
