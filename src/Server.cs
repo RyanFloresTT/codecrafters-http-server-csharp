3e8cfbf09e792cffe5dfd73dcc26ba889a39fa93
Ryan Flores <ryanflorres@gmail.com>
2025-12-17 20:25:18 -0800

    added response and close client

--- src/Server.cs
+++ src/Server.cs
@@ -2,18 +2,80 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
-TcpListener server = new (IPAddress.Any, 4221);
-server.Start();
-Socket socket = server.AcceptSocket(); // wait for client
+string[] endpoints = ["index.html"];
 
-const string httpVersion = "HTTP/1.1";
+TcpListener server = new(IPAddress.Any, 4221);
 
-const string successStatusMessage = "200 OK";
+try {
+    server.Start();
+    Console.WriteLine("Server started");
 
-string headers = string.Empty;
-string body = string.Empty;
+    while (true) {
+        TcpClient client = server.AcceptTcpClient();
+        Console.WriteLine("Client connected!");
 
-string finalMessage = $"{httpVersion} {successStatusMessage}\r\n{headers}\r\n{body}";
+        await Task.Run(() => Serve(client));
+    }
+}
+catch (Exception e) {
+    Console.WriteLine($"Error {e} occurred");
+}
 
-socket.Send(Encoding.ASCII.GetBytes(finalMessage));
-socket.Close();
+server.Dispose();
+return;
+
+async Task Serve(TcpClient client) {
+    NetworkStream stream = client.GetStream();
+
+    byte[] requestBuffer = new byte[1024];
+
+    int bytesRead = await stream.ReadAsync(requestBuffer);
+    if (bytesRead == 0) {
+        Console.WriteLine("No data received from client.");
+        return;
+    }
+
+    string request = Encoding.UTF8.GetString(requestBuffer);
+
+    // GET /index.html HTTP/1.1\r\nHost: localhost:4221\r\nUser-Agent: curl/7.64.1\r\nAccept: */*\r\n\r\n
+    string[] requestParams = request.Split("\r\n");
+
+    // GET /index.html HTTP/1.1\ -> /index.html
+    string address = requestParams[0].Split(" ")[1];
+
+    StringBuilder response;
+
+    if ((address[0] is '/' && endpoints.Contains(address[1..])) || address is "/" or null)
+        response = ResponseBuilder("200", "");
+    else if (address.StartsWith("/echo")) {
+        string randomString = address.Split("/")[2];
+        response = ResponseBuilder("200", randomString);
+    }
+    else if (address.StartsWith("/user-agent")) {
+        string userAgentParam = requestParams[1];
+        string userAgent = userAgentParam.Split(" ")[1];
+        response = ResponseBuilder("200", userAgent);
+    }
+    else
+        response = ResponseBuilder("404", "Not Found");
+
+    byte[] responseData = Encoding.UTF8.GetBytes(response.ToString());
+    await stream.WriteAsync(responseData);
+
+    client.Close();
+    Console.WriteLine("Client disconnected.");
+}
+
+StringBuilder ResponseBuilder(string statusCode, string content) {
+    StringBuilder responseString = new();
+
+    if (statusCode == "404")
+        return responseString.Append($"HTTP/1.1 {statusCode} Not Found\r\n\r\n");
+
+    responseString.Append($"HTTP/1.1 {statusCode} OK\r\n");
+    responseString.Append("Content-Type: text/plain\r\n");
+    responseString.Append($"Content-Length: {content.Length}\r\n\r\n");
+    responseString.Append($"{content}");
+
+    return responseString;
+}
