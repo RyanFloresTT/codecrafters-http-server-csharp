becaa6fb3d36d391de5d5fac1534f07c86803687
Ryan Flores <rryanflorres@gmail.com>
2025-12-17 21:00:26 -0800

    create handler for each tcp connection

--- src/Server.cs
+++ src/Server.cs
@@ -4,20 +4,32 @@ using codecrafters_http_server;
 
 TcpListener server = new(IPAddress.Any, 4221);
 
+// Start the server and accept multiple clients concurrently
 try {
     server.Start();
     Console.WriteLine("Server started");
-    HttpRequestHandler handler = new();
 
     while (true) {
-        TcpClient client = server.AcceptTcpClient();
+        TcpClient client = await server.AcceptTcpClientAsync();
         Console.WriteLine("Client connected!");
 
-        await Task.Run(() => handler.Serve(client));
+        _ = Task.Run(async () => {
+            HttpRequestHandler handler = new();
+            try {
+                await handler.Serve(client);
+            }
+            catch (Exception ex) {
+                Console.WriteLine($"Error while serving a client: {ex.Message}");
+            }
+            finally {
+                client.Close();
+                Console.WriteLine("Client disconnected.");
+            }
+        });
     }
 }
 catch (Exception e) {
-    Console.WriteLine($"Error {e} occurred");
+    Console.WriteLine($"Server error: {e.Message}");
 }
 
 server.Dispose();
